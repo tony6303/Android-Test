61dce53f9778b710646fb2c55f4c93c0657b148c
Lee DaeYeob <tony6303@naver.com>
2021-03-09 11:44:59 +0900

    second commit

--- PhoneApp-android/src/main/java/com/cos/phoneapp/domain/PhoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cos.phoneapp.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneRepository extends JpaRepository<Phone, Long>{
-
-}
